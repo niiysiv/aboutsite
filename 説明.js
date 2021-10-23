837ed70487e0c04fb65d08eaa8269b0f69fc0ef7
niiysiv <75676836+niiysiv@users.noreply.github.com>
2021-10-23 11:38:42 +0900

    Update 説明.js

--- 説明.js
+++ 説明.js
@@ -13,6 +13,15 @@ document.getElementById("js-button1").onclick = function()
       alert('チャンネル登録よろしく')
       location.href = "https://www.youtube.com/channel/UCZeSPZXaTEE-ucFkwoQmZvw"
     }
+    else if(pass === "tamoto")
+    {
+      alert('チャンネル登録よろしく')
+      location.href = "https://www.youtube.com/channel/UCZeSPZXaTEE-ucFkwoQmZvw"
+    }
+    else if(pass === "sin")
+    {
+        alert('シルバー雑魚すぎた裏闘技場'9
+    }
     else
     {
       alert('じゃーんねんでしたーー')
